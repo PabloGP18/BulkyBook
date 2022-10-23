0c74c261ac3b68fb7c044b4f2167f316f105d763
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-10-23 22:03:41 +0200

    Added a static class to the utility project

---
+++ BulkyBook.Utility/SD.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Utility
+{
+    // I have made this so static so you don't have to create an object of this class.
+    // Here it will hold all the static details
+    public static class SD
+    {
+
+    }
+}
